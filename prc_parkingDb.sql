f517c323c19b089fa2a3fd9abfda70fc6e6c5c78
Raziel <robroblemol>
2017-11-12 18:10:54 -0500

    create table plazaMotos

--- prc_parkingDb.sql
+++ prc_parkingDb.sql
@@ -4,28 +4,31 @@ DROP DATABASE IF EXISTS parking;
 create database parking;
 
 DROP TABLE IF EXISTS plazas;
-
 create table plazas (id INT(11)NOT NULL AUTO_INCREMENT,
                       estado BOOLEAN NOT NULL,PRIMARY KEY(id));
+INSERT INTO plazas (estado) VALUES (false);
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 DROP TABLE IF EXISTS vehiculos;
 create table vehiculos (id INT (11) NOT NULL AUTO_INCREMENT,
                         idPlaza INT(11)NOT NULL,placa VARCHAR(15) NOT NULL,
                         fecha_ent DATETIME, PRIMARY KEY (id), FOREIGN KEY(idPlaza) REFERENCES plazas(id));
-
-INSERT INTO plazas (estado) VALUES (false);
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><
 DROP TABLE IF EXISTS nopresentes;
 create table nopresentes (id INT (11) NOT NULL,
                         idPlaza INT(11)NOT NULL,placa VARCHAR(15) NOT NULL,
                         fecha_ent DATETIME, fecha_sal DATETIME, PRIMARY KEY (id));
--- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ALTER TABLE vehiculos ADD tipo BOOLEAN;
 
+
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 DROP PROCEDURE IF EXISTS prc_addVehicule;
 DELIMITER %%
-CREATE PROCEDURE prc_addVehicule (idPlaza int (11), placa VARCHAR (15), fecha_ent DATETIME)
+CREATE PROCEDURE prc_addVehicule (idPlaza int (11), placa VARCHAR (15),
+                                  fecha_ent DATETIME, tipo BOOLEAN)
 BEGIN
-  INSERT INTO  vehiculos (idPlaza,placa,fecha_ent) VALUES (idPlaza,placa,fecha_ent);
+  INSERT INTO  vehiculos (idPlaza,placa,fecha_ent,tipo)
+  VALUES (idPlaza,placa,fecha_ent,tipo);
 END
 %%
 
